e1056408c4981783a55368f343b8b173e84a3028
michele <michele.giovagnoli1@gmail.com>
2023-03-24 16:51:45 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <AppTitle />
+</template>
+
+<script>
+import AppTitle from './components/AppTitle.vue';
+
+export default {
+  components: {
+    AppTitle
+  }
+
+}
+</script>
+
+<style></style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,13 @@
+
+
+<template>
+    <div>ciao</div>
+</template>
+
+<script>
+
+
+
+</script>
+
+<style ></style>
